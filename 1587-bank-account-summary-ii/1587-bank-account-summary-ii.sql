8920879035ef152caa1d55f51ce3a221ea7f16ff
Prabhakar kumar shahi <prabhakar620126@gmail.com>
2026-08-20 00:17:34 +0530

    Time: 428 ms (36.63%), Space: 0B (100%) - LeetHub

---
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -0,0 +1,7 @@
+/* Write your T-SQL query statement below */
+select   u.name , sum(t.amount) as balance
+from users as u 
+left join transactions as t 
+on u.account = t.account 
+group by u.name 
+having sum(t.amount)>10000
