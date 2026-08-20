03d60fda78f62d35c79b88c62d5f1642e7cd3b31
Prabhakar kumar shahi <prabhakar620126@gmail.com>
2026-08-20 15:33:44 +0530

    Time: 346 ms (30.7%), Space: 0B (100%) - LeetHub

---
+++ 3436-find-valid-emails/3436-find-valid-emails.sql
@@ -0,0 +1,9 @@
+/* Write your T-SQL query statement below */
+SELECT user_id , email
+FROM Users
+WHERE email LIKE '[a-zA-Z0-9_]%@[a-zA-Z]%.com' AND
+      email NOT LIKE '%..%' AND 
+      email NOT LIKE '%.%@%' AND
+      email NOT LIKE '%@[a-zA-Z]%[0-9]%' and 
+      email not like '%|%'
+ORDER BY user_id
