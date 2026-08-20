77bba76caa406f1545b46d641d207ddda1966bd3
Prabhakar kumar shahi <prabhakar620126@gmail.com>
2026-08-20 16:06:23 +0530

    Time: 476 ms (5.26%), Space: 0B (100%) - LeetHub

--- 3436-find-valid-emails/3436-find-valid-emails.sql
+++ 3436-find-valid-emails/3436-find-valid-emails.sql
@@ -1,9 +1,18 @@
 /* Write your T-SQL query statement below */
-SELECT user_id , email
+-- SELECT user_id , email
+-- FROM Users
+-- WHERE email LIKE '[a-zA-Z0-9_]%@[a-zA-Z]%.com' AND
+--       email NOT LIKE '%..%' AND 
+--       email NOT LIKE '%.%@%' AND
+--       email NOT LIKE '%@[a-zA-Z]%[0-9]%' and 
+--       email not like '%|%'
+-- ORDER BY user_id
+
+/* Write your T-SQL query statement below */
+SELECT user_id, email
 FROM Users
-WHERE email LIKE '[a-zA-Z0-9_]%@[a-zA-Z]%.com' AND
-      email NOT LIKE '%..%' AND 
-      email NOT LIKE '%.%@%' AND
-      email NOT LIKE '%@[a-zA-Z]%[0-9]%' and 
-      email not like '%|%'
-ORDER BY user_id
+WHERE email LIKE '%_@_%.com'
+  AND email NOT LIKE '%@%@%'
+  AND email NOT LIKE '%[^a-zA-Z0-9_]%@%'
+  AND email NOT LIKE '%@%[^a-zA-Z]%.com'
+ORDER BY user_id ASC;
