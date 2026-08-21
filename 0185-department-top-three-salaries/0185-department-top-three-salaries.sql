21bb6d56f2fe420fa80a561f268b7f5fa73e0fac
Prabhakar kumar shahi <prabhakar620126@gmail.com>
2026-08-22 00:27:34 +0530

    Time: 481 ms (58.88%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,9 @@
+/* Write your T-SQL query statement below */
+with cte as (
+select d.name Department  , e.name as Employee, e.salary as Salary , dense_rank()over(partition by departmentId order by salary desc ) as rn
+from employee  as e 
+left join department as d 
+on e.departmentId =d.id)
+select Department , Employee , Salary
+from cte  
+where rn <=3
