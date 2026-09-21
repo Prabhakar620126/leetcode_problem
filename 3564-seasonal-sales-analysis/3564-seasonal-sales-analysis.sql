79711c8994788e4c28e3c40737c182cafc5764f5
Prabhakar kumar shahi <prabhakar620126@gmail.com>
2026-09-22 00:30:32 +0530

    Time: 466 ms (32.5%), Space: 0B (100%) - LeetHub

--- 3564-seasonal-sales-analysis/3564-seasonal-sales-analysis.sql
+++ 3564-seasonal-sales-analysis/3564-seasonal-sales-analysis.sql
@@ -1,15 +1,39 @@
 /* Write your T-SQL query statement below */
+WITH cte AS (
+    SELECT 
+        s.*,
+        p.category,
+        CASE 
+            WHEN MONTH(sale_date) IN (12, 1, 2) THEN 'Winter'
+            WHEN MONTH(sale_date) IN (3, 4, 5) THEN 'Spring'
+            WHEN MONTH(sale_date) IN (6, 7, 8) THEN 'Summer'
+            ELSE 'Fall'
+        END AS season
+    FROM sales AS s
+    JOIN products AS p
+        ON s.product_id = p.product_id
+),
 
-with cte as (
-select s.* ,p.category,case when month(sale_date) in (12,1,2) then 'Winter'  when month(sale_date) in (3,4,5) then 'Spring' when month(sale_date) in (6,7,8) then 'Summer' else 'Fall'
-end as season
-from sales as s
-join products as p
-on s.product_id=p.product_id),
-cte1 as (
-select season , category , sum(quantity) as total_quantity , sum(quantity*price) as total_revenue , row_number() over(partition by season order by sum(quantity) desc ,sum(quantity*price) desc , category asc)  as rn
-from cte
-group by season , category)
-select season , category , total_quantity , total_revenue 
-from cte1
-where rn=1
+cte1 AS (
+    SELECT 
+        season,
+        category,
+        SUM(quantity) AS total_quantity,
+        SUM(quantity * price) AS total_revenue,
+        ROW_NUMBER() OVER (
+            PARTITION BY season
+            ORDER BY sum(quantity)desc ,
+                SUM(quantity * price) DESC,
+                category ASC
+        ) AS rn
+    FROM cte
+    GROUP BY season, category
+)
+
+SELECT 
+    season,
+    category,
+    total_quantity,
+    total_revenue
+FROM cte1
+WHERE rn = 1;
